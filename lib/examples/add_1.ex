8ef122260d3627a700cbd50d9086057d9304d751
pcmarks <peter.c.marks@gmail.com>
2015-05-25 14:36:55 -0400

    Comment the examples.

--- lib/examples/add_1.ex
+++ lib/examples/add_1.ex
@@ -8,21 +8,17 @@ defmodule Examples.Add1 do
 
   def start do
     {:ok, fbp_graph_reg_name} = Graph.start_link(@graph_1)
+    # Add the components to the graph
     Graph.add_node(fbp_graph_reg_name, @node_1, "Math.Add")
     Graph.add_node(fbp_graph_reg_name, @node_2, "Core.Output")
-    _edge = Graph.add_edge(
-                  fbp_graph_reg_name,
-                  @node_1, :sum,
-                  @node_2, :in_port)
-
+    # Connect the components
+    Graph.add_edge(fbp_graph_reg_name, @node_1, :sum, @node_2, :in_port)
+    # Initialization
     Graph.add_initial(fbp_graph_reg_name, 42, @node_1, :addend)
     Graph.add_initial(fbp_graph_reg_name, 24, @node_1, :augend)
+    # Start the flow
     {:ok, _fbp_network_pid} =
         Network.start_link(fbp_graph_reg_name)
     Network.start()
-    # Cant stop the Network before all the components have executed.
-    # Need a status of paused?
-    #Network.stop()
-    #Network.stop_network()
   end
 end
--- lib/examples/citibike.ex
+++ lib/examples/citibike.ex
@@ -12,33 +12,20 @@ defmodule Examples.Citibike do
 
   def start do
     {:ok, fbp_graph_reg_name} = Graph.start_link(@graph_1)
+    # Add the components to the graph
     Graph.add_node(fbp_graph_reg_name, @node_1, "Streamtools.Ticker")
     Graph.add_node(fbp_graph_reg_name, @node_2, "Streamtools.Map")
     Graph.add_node(fbp_graph_reg_name, @node_3, "Streamtools.GetHTTPJSON")
     Graph.add_node(fbp_graph_reg_name, @node_4, "Streamtools.Unpack")
     Graph.add_node(fbp_graph_reg_name, @node_5, "Streamtools.Filter")
     Graph.add_node(fbp_graph_reg_name, @node_6, "Core.Output")
-    _edge = Graph.add_edge(
-                  fbp_graph_reg_name,
-                  @node_1, :out,
-                  @node_2, :in_port)
-    _edge = Graph.add_edge(
-                  fbp_graph_reg_name,
-                  @node_2, :out,
-                  @node_3, :path)
-    _edge = Graph.add_edge(
-                  fbp_graph_reg_name,
-                  @node_3, :out,
-                  @node_4, :in_port)
-    _edge = Graph.add_edge(
-                  fbp_graph_reg_name,
-                  @node_4, :out,
-                  @node_5, :in_port)
-    _edge = Graph.add_edge(
-                  fbp_graph_reg_name,
-                  @node_5, :out,
-                  @node_6, :in_port)
-
+    # Connect the components
+    Graph.add_edge(fbp_graph_reg_name, @node_1, :out, @node_2, :in_port)
+    Graph.add_edge(fbp_graph_reg_name, @node_2, :out, @node_3, :path)
+    Graph.add_edge(fbp_graph_reg_name, @node_3, :out, @node_4, :in_port)
+    Graph.add_edge(fbp_graph_reg_name, @node_4, :out, @node_5, :in_port)
+    Graph.add_edge(fbp_graph_reg_name, @node_5, :out, @node_6, :in_port)
+    # Set initial values
     Graph.add_initial(fbp_graph_reg_name, 10_000, @node_1, :interval)
     Graph.add_initial(fbp_graph_reg_name,
                       "http://www.citibikenyc.com/stations/json",
@@ -46,13 +33,9 @@ defmodule Examples.Citibike do
     Graph.add_initial(fbp_graph_reg_name, "stationBeanList", @node_4, :part)
     Graph.add_initial(fbp_graph_reg_name, "stationName", @node_5, :filter)
     Graph.add_initial(fbp_graph_reg_name, "W 41 St & 8 Ave", @node_5, :filter_value)
-
+    # Start the flow
     {:ok, _fbp_network_pid} =
         Network.start_link(fbp_graph_reg_name)
     Network.start()
-    # Cant stop the Network before all the components have executed.
-    # Need a wait?
-    #Network.stop()
-    #Network.stop_network()
   end
 end
