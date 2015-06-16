c1699d5eeafffde00fb90f50356e14522b8f830d
pcmarks <peter.c.marks@gmail.com>
2015-06-16 13:29:11 -0400

    Remove debug prints in Math.Add component

--- lib/examples/component/math/add.ex
+++ lib/examples/component/math/add.ex
@@ -10,18 +10,18 @@ defmodule Math.Add do
     # IO.puts("Math.Add.loop(#{inspect augend},#{inspect addend}, #{inspect sum})")
     receive do
       {:augend, value} when addend != nil ->
-        IO.puts("Math.Add: {:augend, #{value}}")
+        # IO.puts("Math.Add: {:augend, #{value}}")
         sum = ElixirFBP.Component.send_ip(sum, addend + value)
         loop(nil, nil, sum)
       {:augend, value} ->
-        IO.puts("Math.Add: {:augend, #{value}}")
+        # IO.puts("Math.Add: {:augend, #{value}}")
         loop(value, addend, sum)
       {:addend, value} when augend != nil ->
-        IO.puts("Math.Add: {:addend, #{value}}, augend: #{augend}")
+        # IO.puts("Math.Add: {:addend, #{value}}, augend: #{augend}")
         sum = ElixirFBP.Component.send_ip(sum, value + augend)
         loop(nil, nil, sum)
       {:addend, value} ->
-        IO.puts("Math.Add: {:addend, #{value}}")
+        # IO.puts("Math.Add: {:addend, #{value}}")
         loop(augend, value, sum)
     end
   end
