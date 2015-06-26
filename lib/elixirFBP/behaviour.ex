78834043b647d80ffc295ea7402bedad6b0c607d
pcmarks <peter.c.marks@gmail.com>
2015-06-26 13:03:34 -0400

    Update ElixirFBP.Behaviour

--- lib/elixirFBP/behaviour.ex
+++ lib/elixirFBP/behaviour.ex
@@ -1,8 +1,9 @@
 defmodule ElixirFBP.Behaviour do
   use Behaviour
 
-  defcallback inports() :: [ElixirAtom: ElixirAtom]
-  defcallback outports() :: [ElixirAtom: ElixirAtom]
+  defcallback description :: String.t
+  defcallback inports() :: [atom: atom]
+  defcallback outports() :: [atom: atom]
   defcallback loop(%{}, %{}) :: any
-  
+
 end
