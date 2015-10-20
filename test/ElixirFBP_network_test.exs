b32f7f9606bf9e399dc58752ec0a049df9c74d05
pcmarks <peter.c.marks@gmail.com>
2015-10-20 09:08:28 -0400

    New network_tests.
    Network tests for push and pull running modes.

--- test/ElixirFBP_network_test.exs
+++ test/ElixirFBP_network_test.exs
@@ -48,4 +48,48 @@ defmodule ElixirFBPNetworkTest do
     Network.stop(@graph_1)
   end
 
+  test "Create and start an ElxirFBP Graph in push mode" do
+    Network.start_link
+    {:ok, fbp_graph} = Network.clear(@graph_1)
+    Graph.add_node(fbp_graph, @node_1, "Math.Add")
+    Graph.add_node(fbp_graph, @node_2, "Core.Log")
+    _edge = Graph.add_edge(
+                  fbp_graph,
+                  @node_1, :sum,
+                  @node_2, :in_port)
+
+    Graph.add_initial(fbp_graph, 53, @node_1, :addend)
+    Graph.add_initial(fbp_graph, 35, @node_1, :augend)
+    assert capture_log(fn ->
+      Network.start(@graph_1, :push)
+      # Need a slight delay to allow for the log message to shou up
+      :timer.sleep(5)
+    end) =~ "88"
+    {true, true} = Network.get_status(@graph_1)
+    # Make sure the graph and network are stopped
+    Network.stop(@graph_1)
+  end
+
+  test "Create and start an ElxirFBP Graph in pull mode" do
+    Network.start_link
+    {:ok, fbp_graph} = Network.clear(@graph_1)
+    Graph.add_node(fbp_graph, @node_1, "Math.Add")
+    Graph.add_node(fbp_graph, @node_2, "Core.Log")
+    _edge = Graph.add_edge(
+                  fbp_graph,
+                  @node_1, :sum,
+                  @node_2, :in_port)
+
+    Graph.add_initial(fbp_graph, 32, @node_1, :addend)
+    Graph.add_initial(fbp_graph, 23, @node_1, :augend)
+    assert capture_log(fn ->
+      Network.start(@graph_1, :pull)
+      # Need a slight delay to allow for the log message to shou up
+      :timer.sleep(5)
+    end) =~ "55"
+    {true, true} = Network.get_status(@graph_1)
+    # Make sure the graph and network are stopped
+    Network.stop(@graph_1)
+  end
+
 end
