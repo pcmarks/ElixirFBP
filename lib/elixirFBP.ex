4a28b44a7a2bed446239c0a425ae3ca7c11458bb
pcmarks <peter.c.marks@gmail.com>
2015-10-20 09:56:07 -0400

    More documentation changes.

--- lib/elixirFBP.ex
+++ lib/elixirFBP.ex
@@ -6,10 +6,12 @@ defmodule ElixirFBP do
   IPs - typically after some computation - out the out ports.
 
   In ElixirFBP, each component is a module that must specify the names of its
-  in and out ports and it must implement a loop function.
+  in and out ports and it must implement a loop function that is responsible for
+  maintaining state and sending computed values to the out ports.
 
-  An FBP program is created by adding components to an ElixrFBP.Graph. The
-  graph, in turn, is kept inside and managed by an ElixirFBP.Network.
-   
+  An FBP program is created by adding components to an ElixrFBP.Graph (a gen_server).
+  The graph, in turn, is kept inside and managed by an ElixirFBP.Network
+  (a gen_server).
+
   """
 end
